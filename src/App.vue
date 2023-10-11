e839f8a55087501198815371f527ac41ff6d97f3
matsu <matsu@NOTEBOOK-ESCRIT>
2023-10-11 07:45:12 -0300

    Add Router Link

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,86 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-<div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">Menu</span>
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                            <i class="fi fi-rr-home"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-user-add"></i> <span class="ms-1 d-none d-sm-inline">Cadastrar</span> </a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                            </li> -->
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-usd-circle"></i> <span class="ms-1 d-none d-sm-inline">Gerar Comissão</span></a>
-                    </li>
-                    <li>
-                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                            <i class="fi fi-rr-chart-histogram"></i> <span class="ms-1 d-none d-sm-inline">Faturamento</span></a>
-                        <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
-                            </li> -->
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-hand-holding-usd"></i> <span class="ms-1 d-none d-sm-inline">Gastos Frota</span> </a>
-                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                            </li> -->
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-add"></i> <span class="ms-1 d-none d-sm-inline">Despesas Frota</span> </a>
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                            </li> -->
-                    </li>
-
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-search-dollar"></i> <span class="ms-1 d-none d-sm-inline">Recebimento</span> </a>
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                            </li> -->
-                    </li>
-
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fi fi-rr-hand-holding-medical"></i> <span class="ms-1 d-none d-sm-inline">Atendimento</span> </a>
-                            <!-- <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                            </li> -->
-                    </li>
-                </ul>
-                <hr>
+    <nav>
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <router-link to="/" class="navbar-brand ps-3">Adm - Master</router-link>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading"></div>
+                            <a class="nav-link">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+                            </a>
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-user-add"></i></div>
+                                <router-link to="/colaborador" class="nav-link">Colaboradores</router-link> 
+                            </a>
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-usd-circle"></i></div>
+                                <router-link to="/" class="nav-link">Gerar Comissões</router-link>
+                            </a> 
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-chart-histogram"></i></div>
+                                <router-link to="/" class="nav-link">Faturamento</router-link> 
+                            </a>
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-hand-holding-usd"></i></div>
+                                <router-link to="/" class="nav-link">Gastos da Frota</router-link> 
+                            </a>
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-add"></i></div>
+                                <router-link to="/" class="nav-link">Despesas de Atendimento</router-link> 
+                            </a>
+                            <a class="nav-link">
+                                <div><i class="fi fi-rr-search-dollar"></i></div>
+                                <router-link to="/" class="nav-link">Recebimento de Comissão</router-link> 
+                            </a>
+                        </div>
+                    </div>
+                </nav>
             </div>
+
+            <div id="layoutSidenav_content">
+                <div class="main">
+            <router-view></router-view>
         </div>
-        <div class="col py-3">
-            Content area...
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div> 
         </div>
-    </div>
-</div>
+
+    </nav>
 </template>
