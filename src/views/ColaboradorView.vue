88d2c9461907c431480856da274fcf998b6f41a4
FERNANDO-MATSUHASHI <matsu.zf@gmail.com>
2023-10-18 00:39:47 -0300

    Arrumado Menu

--- src/views/ColaboradorView.vue
+++ src/views/ColaboradorView.vue
@@ -2,42 +2,52 @@
 </script>
 
 <template>
-<div class="row">
-  <div class="col-1 d-flex justify-content-center mt-3">
-    <i class="fi fi-rr-user-add"></i>
+  <main class="colaborador">
+    <div class="row">
+      <div class="d-flex justify-content">
+        <i style="font-size: 40px; margin-left: 30px;" class="fi fi-rr-user-add"></i>
+        <div style="margin-left: 20px;" class="col">
+          <h1>Colaboradores</h1>
+        </div>
+      </div>
     </div>
-    <div class="col">
-      <h1>Colaboradores</h1>
-    </div>
-</div>
-  <hr>
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    <hr>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
+
+<style>
+.colaborador {
+  margin-top: 100px;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+</style>
